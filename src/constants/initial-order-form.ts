ddfa47f2beb940f87be17027a7e39d9d9daade48
Hevelson <hevelson@gmail.com>
2022-08-01 21:53:06 -0300

    feat(constants): created initial order form data

---
+++ src/constants/initial-order-form.ts
@@ -0,0 +1,15 @@
+const initialOrderForm = {
+  clientId: '',
+  code: '',
+  deviceType: '',
+  brand: '',
+  model: '',
+  serialNumber: '',
+  orderType: '',
+  status: '',
+  amount: 0,
+  defect: '',
+  createdAt: new Date(),
+};
+
+export default initialOrderForm;
